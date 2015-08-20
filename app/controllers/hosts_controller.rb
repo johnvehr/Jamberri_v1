5175eb770c2bbf72a15ad65de62f7debc5fda57a
johnvehr <johnvehr@yahoo.com>
2015-08-20 14:24:12 -0400

    add user id to host params

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -70,6 +70,6 @@ class HostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def host_params
-      params.require(:host).permit(:host_title, :host_bio)
+      params.require(:host).permit(:host_title, :host_bio, :user_id)
     end
 end
