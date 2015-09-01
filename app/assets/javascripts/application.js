de46a807b562da392735587d21588ad6b870af2e
johnvehr <johnvehr@yahoo.com>
2015-09-01 10:42:39 -0600

    move js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,21 @@
 //= require turbolinks
 //= require materialize-sprockets
 //= require_tree .
+
+
+$(document).ready(function(){
+  var x = document.getElementById("demo");
+ function runThisTown(){
+   $.get("http://ipinfo.io", function(response) {
+     console.log(response.city, response.country);
+     x.innerHTML = response.city + ", <small>"+response.loc+"</small>";
+   }, "jsonp");
+ }
+ runThisTown();
+  $(".button-collapse").sideNav();
+  $('.parallax').parallax();
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+})
